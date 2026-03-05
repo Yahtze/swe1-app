de1dc50d3d760f9f5bd800de0b62b025e7cd808d
Duvuru Yathin Reddy <yathinreddy2004@gmail.com>
2026-03-04 23:48:54 -0500

    Added Basic Poll

---
+++ polls/migrations/0002_basic_poll.py
@@ -0,0 +1,31 @@
+from django.db import migrations
+from django.utils import timezone
+
+def create_basic_poll(apps, schema_editor):
+    Question = apps.get_model('polls', 'Question')
+    Choice = apps.get_model('polls', 'Choice')
+    
+    question_text = "What is your favorite programming language?"
+    if not Question.objects.filter(question_text=question_text).exists():
+        q = Question.objects.create(
+            question_text=question_text,
+            pub_date=timezone.now()
+        )
+        Choice.objects.create(question=q, choice_text="Python")
+        Choice.objects.create(question=q, choice_text="JavaScript")
+        Choice.objects.create(question=q, choice_text="C++")
+        Choice.objects.create(question=q, choice_text="Rust")
+
+def delete_basic_poll(apps, schema_editor):
+    Question = apps.get_model('polls', 'Question')
+    Question.objects.filter(question_text="What is your favorite programming language?").delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_basic_poll, delete_basic_poll),
+    ]
